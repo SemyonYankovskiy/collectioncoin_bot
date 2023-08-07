0ed8134cf2db40c3dc2411c14e295deb3b6a299b
Сименс <semyon21@mail.ru>
2023-08-07 16:09:13 +0300

    v2.0 рабочий контейнер

--- settngs.py
+++ settngs.py
@@ -6,7 +6,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from database import db_connection
 
 
-API_TOKEN = os.getenv("TG_TOKEN")
+API_TOKEN = "6180484344:AAEUcNi_AFUIj_ePD843UKhFWZuo1SOp1QE"
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
