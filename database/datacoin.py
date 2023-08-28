c016624e4ea420a9536a77e4c66c84f98b0fa1c9
Сименс <semyon21@mail.ru>
2023-08-28 21:00:52 +0300

    v2.2.1

--- database/datacoin.py
+++ database/datacoin.py
@@ -14,9 +14,7 @@ class DataCoin:
 
     def save(self):
         try:
-            print("----------------------")
             print(datetime.now(), "| ", "Удаление из базы данных")
-            print(self.datetime.strftime('%Y.%m.%d'))
             db.cursor.execute(
                 f"DELETE FROM graph_data WHERE tg_id='{self.telegram_id}' "
                 f"and datetime='{self.datetime.strftime('%Y.%m.%d')}'"
