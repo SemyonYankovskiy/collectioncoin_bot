48a7beae35f875a9071db896ab1b3a403c271f24
syankovskiy <semyon21@mail.ru>
2023-08-29 10:42:09 +0300

    1

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -12,6 +12,6 @@ from .coin_info import output_counties, output_eurocoin, output_coin, swap
 from .graph import grafik
 from .map import maps, process_callback_map
 from .misc import hello_welcome, ua_welcome
-from .misc import unknown
 from .services import refresh_data, summ
 from .top10 import top10_default, top10_by_sort
+from .misc import unknown
