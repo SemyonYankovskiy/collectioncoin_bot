6b8f86b7f0c12c924e3caf107cb4aad43b90c66b
syankovskiy <semyon21@mail.ru>
2024-01-22 22:08:19 +0300

    v.2.8 опять тг-токен закомитил. в предыдущем комите добалвен task по времени с 10 до 20

--- settngs.py
+++ settngs.py
@@ -6,7 +6,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from database import db_connection
 
 
-API_TOKEN = "6180484344:AAH0Ypqp090qja_ovPdzFwROifdyZ2CzlLI"
+API_TOKEN = os.getenv("TG_TOKEN")
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
