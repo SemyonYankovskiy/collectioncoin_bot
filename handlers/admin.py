5bcca44e0902096265bc6d00451dc4044cb691bf
syankovskiy <semyon21@mail.ru>
2024-02-12 10:38:42 +0300

    v.2.9.4 добалвена функция отправки сообщения всем пользователям. Доабвлено оповещение о перезагрузке в Run

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,12 +1,19 @@
+import asyncio
+import sqlite3
 from datetime import datetime
 
+from aiogram.utils import exceptions
+
 from core.types import MessageWithUser
+from database import User
 from handlers import profile
 from handlers.graph import grafik
-from handlers.map import maps
 from handlers.services import refresh_data, summ, _summ
 from helpers.handler_decorators import check_and_set_user
-from settngs import dp
+from settngs import dp, bot
+from database import DataCoin, User
+from aiogram import types
+
 
 
 @dp.message_handler(commands=["all"])
@@ -17,4 +24,41 @@ async def all_(message: MessageWithUser):
     await profile(message)
     await _summ(message)
     await grafik(message)
+
+
+
+
+
+async def send_to_all_users(message_text= "Бот был перезагружен, обновите данные \n /refresh"):
+    # Получаем список всех пользователей из базы данных
+    users_list = User.get_all()
+
+
+    # Отправляем сообщение каждому пользователю
+    for user in users_list:
+        print(user.telegram_id)
+        await send_message_to_users_handler(user.telegram_id, message_text)
+        await asyncio.sleep(0.1)
+
+
+# Вспомогательная функция для безопасной отправки сообщения
+async def send_message_to_users_handler(user_id: int, text: str, disable_notification: bool = False) -> bool:
+    try:
+        await bot.send_message(user_id, text, disable_notification=disable_notification)
+    except exceptions.BotBlocked:
+        print(f"Пользователь [ID:{user_id}] заблокировал бота")
+    except exceptions.ChatNotFound:
+        print(f"Неверный ID пользователя [ID:{user_id}]")
+    except exceptions.RetryAfter as e:
+        print(f"Превышен лимит отправки сообщений для [ID:{user_id}]. Подождите {e.timeout} секунд.")
+        await asyncio.sleep(e.timeout)
+        return await send_message_to_users_handler(user_id, text)
+    except exceptions.UserDeactivated:
+        print(f"Пользователь [ID:{user_id}] деактивирован")
+    except exceptions.TelegramAPIError:
+        print(f"Не удалось отправить сообщение пользователю [ID:{user_id}]")
+    else:
+        print(f"Сообщение успешно отправлено пользователю [ID:{user_id}]")
+        return True
+    return False
 
--- run.py
+++ run.py
@@ -3,11 +3,14 @@ from threading import Thread
 
 from aiogram import executor
 
+from handlers.admin import send_to_all_users
 from settngs import dp
 from core.gather import gather_manager
 import handlers  # инициализация хэндлеров
 from core.notifications.task import new_notifications_checker
 
+async def on_startup(dp):
+    await send_to_all_users()
 
 # Запускаем бота
 if __name__ == "__main__":
@@ -16,7 +19,9 @@ if __name__ == "__main__":
 
     print("\nImport: ", handlers)
 
-    # loop = asyncio.get_event_loop()
+    loop = asyncio.get_event_loop()
+
     # loop.create_task(new_notifications_checker())
+    loop.run_until_complete(on_startup(dp))
 
     executor.start_polling(dp, skip_updates=True)
